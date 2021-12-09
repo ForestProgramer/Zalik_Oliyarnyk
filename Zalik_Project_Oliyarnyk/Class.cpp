0019146e6756dcce929bc4be6b4d42a89eaf1a33
Max <maksymoliiarnyk@nltu.lviv.ua>
2021-12-09 13:37:27 +0200

    added code br2

--- Zalik_Project_Oliyarnyk/Class.cpp
+++ Zalik_Project_Oliyarnyk/Class.cpp
@@ -1,5 +1,19 @@
 #include "Class.h"
-class className {
-	// some data
-	// some functions
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
 };
+
+int main() {
+  MyClass myObj;  // Create an object of MyClass
+
+  // Access attributes and set values
+  myObj.myNum = 15; 
+  myObj.myString = "Some text";
+
+  // Print attribute values
+  cout << myObj.myNum << "\n"; 
+  cout << myObj.myString; 
+  return 0;
+}
